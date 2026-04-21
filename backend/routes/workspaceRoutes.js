040c2ee564b883e3a2ee9bb71d979e7037665b2f
nikita2107 <113438754+nikita2107@users.noreply.github.com>
2026-04-21 17:01:09 +0530

    working website

--- backend/routes/workspaceRoutes.js
+++ backend/routes/workspaceRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { getWorkspaces, createWorkspace, addMember } from "../controllers/workspaceController.js";
+import { getWorkspaces, addMember } from "../controllers/workspaceController.js";
 
 const router = express.Router();
 
-router.get("/",         getWorkspaces);
-router.post("/",        createWorkspace); // NEW: create workspace
+router.get("/", getWorkspaces);
 router.post("/members", addMember);
 
 export default router;
